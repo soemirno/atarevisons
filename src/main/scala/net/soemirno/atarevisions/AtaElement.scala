f0355bb94439ebb16ec1e1a8eb6c74e0c35cd0dd
soemirno <soemirno@gmail.com>
2009-06-01 14:49:00 +0200

    work on readibility

--- src/main/scala/net/soemirno/atarevisions/AtaElement.scala
+++ src/main/scala/net/soemirno/atarevisions/AtaElement.scala
@@ -1,8 +1,7 @@
 package net.soemirno.atarevisions
 
-import collection.mutable.HashMap
 import java.io.File
-import xml.{NodeSeq, Node, Elem}
+import xml.Elem
 
 object AtaElement {
   def apply(elem: Elem) = {
@@ -29,58 +28,72 @@ object AtaElement {
 class AtaElement(elem: Elem) {
 
   private val revIndicators = {
-    val keyedElems = (elem \\ "_") filter (element => element \ "@chg" != "")
+    val elementsContainingRevInd = (elem \\ "_").filter(e => e \ "@chg" != "")
     val list = new RevisionIndicators
-    for (element <- keyedElems)
-      list add (RevisionIndicator(element))
+
+    for (element <- elementsContainingRevInd) list.add (RevisionIndicator(element))
     list
   }
 
-  def element(): Elem = elem
-
   def revisionIndicators(): RevisionIndicators = revIndicators
 
-  def diff(other: AtaElement, revisionDate: String): RevisionIndicators = {
-    val visitedList = new RevisionIndicators
-    val prevChanges = other.revisionIndicators
-    visitedList ++ findNew(prevChanges, revisionDate, visitedList)
-    visitedList ++ findChangedLength(prevChanges, revisionDate, visitedList)
-    visitedList ++ findChangedEffectivity(prevChanges, revisionDate, visitedList)
-    visitedList ++ findChangedInChildren(prevChanges, revisionDate, visitedList)
-    visitedList ++ findDeleted(prevChanges, revisionDate, visitedList)
-    return visitedList
+  def diff(previous: AtaElement, revisionDate: String): RevisionIndicators = {
+    val result = new RevisionIndicators
+    val previousIndicators = previous.revisionIndicators
+
+    result ++ findNew ( previousIndicators, revisionDate, result)
+    result ++ findLengthChanges ( previousIndicators, revisionDate, result)
+    result ++ findEffectivityChanges ( previousIndicators, revisionDate, result)
+    result ++ findChildrenChanges ( previousIndicators, revisionDate, result)
+    result ++ findDeleted ( previousIndicators, revisionDate, result)
+
+    return result
   }
 
-  def findNew(prevChanges: RevisionIndicators, revisionDate: String, visitedList: RevisionIndicators): RevisionIndicators = {
+  def findNew(prevChanges: RevisionIndicators,
+              revisionDate: String,
+              foundChanges: RevisionIndicators): RevisionIndicators = {
     val result = new RevisionIndicators
-    for (rev <- revIndicators.values if !visitedList.contains(rev.key)) {
-      Console.println("comparing new " + rev.key)
+
+    for (rev <- revIndicators.values if !foundChanges.contains(rev.key)) {
+      Console.println("finding new " + rev.key)
+
       if (!prevChanges.contains(rev.key()) || prevChanges(rev.key()).changeType == "D")
         result add Some(RevisionIndicator(rev.key, "N", revisionDate, rev.element))
     }
-    result
+
+    return result
   }
 
-  def findDeleted(prevChanges: RevisionIndicators, revisionDate: String, visitedList: RevisionIndicators): RevisionIndicators = {
+  def findDeleted(prevChanges: RevisionIndicators,
+                  revisionDate: String,
+                  foundChanges: RevisionIndicators): RevisionIndicators = {
     val result = new RevisionIndicators
-    for (rev <- prevChanges.values if !visitedList.contains(rev.key)) {
-      Console.println("comparing deleted " + rev.key)
+
+    for (rev <- prevChanges.values if !foundChanges.contains(rev.key)) {
+      Console.println("finding deleted " + rev.key)
+
       if (!revIndicators.contains(rev.key()))
         result add Some(RevisionIndicator(rev.key, "D", revisionDate, rev.element))
     }
-    result
+
+    return result
   }
 
-  def findChangedEffectivity(prevChanges: RevisionIndicators, revisionDate: String, visitedList: RevisionIndicators): RevisionIndicators = {
+  def findEffectivityChanges(prevChanges: RevisionIndicators,
+                             revisionDate: String,
+                             foundChanges: RevisionIndicators): RevisionIndicators = {
     val result = new RevisionIndicators
-    for (rev <- revIndicators.values if !visitedList.contains(rev.key)) {
-      Console.println("comparing effectivities " + rev.key)
+
+    for (rev <- revIndicators.values if !foundChanges.contains(rev.key)) {
+      Console.println("finding effectivity changes " + rev.key)
 
       if (hasEffectivityChanges(rev.element, prevChanges(rev.key).element))
         result add Some(RevisionIndicator(rev.key, "R", revisionDate, rev.element))
 
     }
-    result
+
+    return result
   }
 
   def hasEffectivityChanges(current: Elem, previous: Elem): Boolean = {
@@ -89,42 +102,46 @@ class AtaElement(elem: Elem) {
 
     if (currentEffectElem.size == 0 && previousEffectElem.size == 0) return false
 
-    val currentEffectivity = (currentEffectElem \ "@effrg").text
-    val previousEffectivity = (previousEffectElem \ "@effrg").text
+    val currentEffectivity = currentEffectElem \ "@effrg"
+    val previousEffectivity = previousEffectElem \ "@effrg"
+
     if (currentEffectivity != previousEffectivity) return true
 
-    val currentSB = (currentEffectElem \ "sbeff")
-    val previousSB = (previousEffectElem \ "sbeff")
+    val currentSB = currentEffectElem \ "sbeff"
+    val previousSB = previousEffectElem \ "sbeff"
 
     if (currentSB.size != previousSB.size) return true
 
     for (sb <- currentSB) {
       val prevSbItem = previousSB.filter(
-        n => (n \ "@sbnbr").text == (sb \ "@sbnbr").text &&
-             (n \ "@sbcond").text == (sb \ "@sbcond").text
+        n => n \ "@sbnbr" == sb \ "@sbnbr" &&
+             n \ "@sbcond" == sb \ "@sbcond"
         )
       if (prevSbItem.size == 0) return true
-      if ((prevSbItem \ "@effrg").text != (sb \ "@effrg").text) return true
+      if (prevSbItem \ "@effrg" != sb \ "@effrg") return true
     }
 
-    val currentCoc = (currentEffectElem \ "coceff")
-    val previousCoc = (previousEffectElem \ "coceff")
+    val currentCoc = currentEffectElem \ "coceff"
+    val previousCoc = previousEffectElem \ "coceff"
 
     if (currentCoc.size != previousCoc.size) return true
 
     for (coc <- currentCoc) {
-      val prevCocItem = previousCoc.filter(n => (n \ "@cocnbr").text == (coc \ "@cocnbr").text)
+      val prevCocItem = previousCoc.filter(n => n \ "@cocnbr" == coc \ "@cocnbr")
       if (prevCocItem.size == 0) return true
-      if ((prevCocItem \ "@effrg").text != (coc \ "@effrg").text) return true
+      if (prevCocItem \ "@effrg" != coc \ "@effrg") return true
     }
 
     return false
   }
 
-  def findChangedLength(prevChanges: RevisionIndicators, revisionDate: String, visitedList: RevisionIndicators): RevisionIndicators  = {
+  def findLengthChanges(prevChanges: RevisionIndicators,
+                        revisionDate: String,
+                        foundChanges: RevisionIndicators): RevisionIndicators  = {
     val result = new RevisionIndicators
-    for (rev <- revIndicators.values if !visitedList.contains(rev.key)) {
-      Console.println("comparing length " + rev.key)
+
+    for (rev <- revIndicators.values if !foundChanges.contains(rev.key)) {
+      Console.println("finding length changes" + rev.key)
 
       val currentLength = rev.children.size
       val previousLength = prevChanges(rev.key).children.size
@@ -133,38 +150,51 @@ class AtaElement(elem: Elem) {
         result add Some(RevisionIndicator(rev.key, "R", revisionDate, rev.element))
 
     }
-    result
+
+    return result
   }
 
-  def findChangedInChildren(prevChanges: RevisionIndicators, revisionDate: String, visitedList: RevisionIndicators): RevisionIndicators =  {
+  def findChildrenChanges(prevChanges: RevisionIndicators,
+                          revisionDate: String,
+                          foundChanges: RevisionIndicators): RevisionIndicators =  {
     val result = new RevisionIndicators
-    for (rev <- revIndicators.values if !visitedList.contains(rev.key)) {
-      Console.println("comparing children " + rev.key)
-      if (childHasChanged(rev, prevChanges, revisionDate, visitedList))
+
+    for (rev <- revIndicators.values if !foundChanges.contains(rev.key)) {
+      Console.println("finding children changes" + rev.key)
+
+      if (childHasChanged(rev, prevChanges, revisionDate, foundChanges))
         result add Some(RevisionIndicator(rev.key, "R", revisionDate, rev.element))
     }
-    result
+    return result
   }
 
-  def childHasChanged(parent: RevisionIndicator, prevChanges: RevisionIndicators, revisionDate: String, visitedList: RevisionIndicators): Boolean = {
-    for (child <- parent.children if (child \ "@chg" != "")) {
+  def childHasChanged(parent: RevisionIndicator,
+                      prevChanges: RevisionIndicators,
+                      revisionDate: String,
+                      foundChanges: RevisionIndicators): Boolean = {
+    for (child <- parent.children if child \ "@chg" != "") {
       val key = (child \ "@key").text
-      if (visitedList.contains(key)) return true
-      if (childHasChanged(revIndicators(key), prevChanges, revisionDate, visitedList)) {
-        return true
-      }
-    }
-    val previousChildren = prevChanges(parent.key).children.filter(n => n \ "@chg" == "")
 
+      if (foundChanges.contains(key)) return true
+
+      if (childHasChanged(revIndicators(key), prevChanges, revisionDate, foundChanges)) return true
+      
+    }
+
+    val previousChildren = prevChanges(parent.key).children.filter(n => n \ "@chg" == "")
     var index = 0
-    for (child <- parent.children if (child \ "@chg" == "")) {
+
+    for (child <- parent.children if child \ "@chg" == "") {
       val prevChild = previousChildren(index)
+
       if (prevChild.label != child.label) return true
+
       if (prevChild.text.trim != child.text.trim) return true
+
       index = index + 1
     }
-    return false
 
+    return false
   }
 
 }
